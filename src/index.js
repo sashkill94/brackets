b25595d2532b74a4bd2c166e6175745afb6d7d3d
sashkill94 <sashkill94@gmail.com>
2022-08-08 18:23:21 +0300

    init: solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const stack = [];
+    for (let i = 0; i < str.length; i++){
+        for (const el of bracketsConfig) {
+          if(el[1] === str[i]){
+            const lastInStack = stack.length > 0 ? stack[stack.length-1] : undefined;
+            if(el[0] === el[1] && (lastInStack != el[0] || lastInStack == undefined)){    
+            } else if(lastInStack != el[0] || lastInStack == undefined){        
+                return false;
+            } else {
+                stack.pop();
+                continue;
+            }
+          }
+          if(el[0] === str[i]){
+            stack.push(str[i]);
+          }
+        }
+      }
+      return stack.length === 0;
 }
